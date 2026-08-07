24d298ba5895c5d723ab075ab306f59031652dac
jyoshikadommeti <jyoshikadommeti2008@gmail.com>
2026-08-07 22:17:48 +0530

    Connect Flask with HTML

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-
+from flask import Flask,
+remder_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to AB InterVue AI"
+    return
+render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
